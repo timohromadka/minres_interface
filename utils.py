db94fe9213be2030ede855b8e5196b8a74083845
Timo Hromadka <timo.hromadka@gmail.com>
2025-06-05 17:43:08 +0200

    Add vlips

--- utils.py
+++ utils.py
@@ -25,7 +25,8 @@ class VideoSample:
         self.filename = self.filepath.split('/')[-1]
         self.label = label if label else self.get_label()
         # self.transform = random.choice(['none', 'h_flip', 'v_flip', 'hv_flip'])
-        self.transform = 'none'
+        # self.transform = 'none'
+        self.transform = random.choice(['none', 'v_flip'])
         self.original_filename = os.path.basename(self.filepath).split("_")[0] + ".mp4" # extract the name before the suffix, with .mp4 at the end
 
     def get_label(self):
